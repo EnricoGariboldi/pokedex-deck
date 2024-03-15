bd8dfbf3cbe67943d78951f6e84007876a920c7a
Enrico Gariboldi <gariboldi.enrico99@gmail.com>
2024-03-15 22:33:38 +0100

    init

---
+++ src/Components/MainPage/MainPage.tsx
@@ -0,0 +1,11 @@
+import { MainPageContainer } from "./StyledComponents";
+
+function MainPage() {
+  return (
+    <MainPageContainer>
+      ciao
+    </MainPageContainer>
+  );
+}
+
+export default MainPage;
